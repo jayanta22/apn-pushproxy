9d5db1da89ebd35e3306b5c8ec2125d78c4a39e3
Piotr Duda <piotr.duda@polidea.pl>
2011-10-03 10:49:28 +0200

    Added missing file

--- apn_pushproxy.py
+++ apn_pushproxy.py
@@ -8,19 +8,34 @@ import random
 import time
 import ssl
 import struct
+import yaml
 import simplejson as json
 from flask import Flask, request
+from hashlib import sha256
 
-
+# The output log file
 LOG_FILE = os.path.expanduser("~/pushproxy.log")
+
+# Server's configuration file
+CONFIG_FILE = "config.yaml"
+
+# Loads all the configiration options
+try:
+    config = yaml.load(open(CONFIG_FILE))
+except IOError,e:
+    print "The configuration file appears to be missing!"
+
+
+# Used as a digital signature, to limit the service users
+SECRET_KEY = config['security_key']
 
 # Host name and certificate for the production APN servers
 APN_PRODUCTION_HOST = ('gateway.push.apple.com', 2195)
-APN_PRODUCTION_CERT = 'prod_certificate.pem'
+APN_PRODUCTION_CERT = config['production_cert'] #prod_certificate.pem'
  
 # Host name and certificate for sandboxed APN servers
 APN_SANDBOX_HOST = ('gateway.sandbox.push.apple.com', 2195)
-APN_SANDBOX_CERT = 'dev_certificate.pem'
+APN_SANDBOX_CERT = config['development_cert'] #dev_certificate.pem'
 
 
 APN_ERRORS = {
@@ -46,7 +61,6 @@ def open_ssl_socket(host, cert):
     ssl_s.connect(host)
     return ssl_s
 
-
 def send_apn(data_socket, notification):
     # Be sure to send the full notification content.
     # Due to GIL no synchronization is needed.
@@ -55,6 +69,7 @@ def send_apn(data_socket, notification):
         notification = notification[num_bytes:]
 
 def safe_send_apn(notification, use_sandbox=False, retries=3):
+    ''' Send the APN notifications, retrying if necessary '''
     
     while retries:
         # Pick the socket to send data to
@@ -78,7 +93,7 @@ def safe_send_apn(notification, use_sandbox=False, retries=3):
                 return "Unexpected APN status repsonse (%s)" % status
 
         except socket.timeout:
-            return "Timeout"
+            return "TIMEOUT"
 
         except socket.error, e:
             code,msg = e
@@ -87,10 +102,9 @@ def safe_send_apn(notification, use_sandbox=False, retries=3):
                 sandbox_socket.close()
                 init_apn_sockets()
             else:
-                return "Error"
+                return "ERROR"
 
         retries -= 1
-
 
 def init_apn_sockets():
     ''' 
@@ -106,6 +120,15 @@ def init_apn_sockets():
         logging.exception( "Cant open ssl sockets")
         logging.exception("Shutting down...")
         sys.exit(1)
+
+def check_signature(payload, client_sign):
+    ''' Checks the digital signature sent with the remote request '''
+
+    calculated_sign = sha256(payload + SECRET_KEY).hexdigest()
+    logging.debug(calculated_sign)
+    if calculated_sign != client_sign:
+        return False
+    return True
 
 
 ######################### REQUEST HANDLERS ############################
@@ -129,12 +152,18 @@ def help():
 @app.route("/", methods = ['POST'])
 def main():
     ''' Sends APN push notifications for Apphance Guide '''
-    
+
     use_sandbox = request.form.get("sandboxed", False)
     device_tokens = request.form.get('device_tokens')
     payload = request.form.get('payload')
     expiration = request.form.get('expiration', 86400)
+    signature = request.form.get('signature')
 
+    # Check the shared secret key
+    if not check_signature(payload, signature):
+        return "BAD_SIGNATURE"
+    
+    
     logging.debug(payload)
     provider_identifier = random.randint(0, 2**16)
     expiry = time.time() + int(expiration)
@@ -142,27 +171,31 @@ def main():
 
     # Validate the input data
     if not device_tokens: 
-        return "Device Tokens are missing!"
+        return "BAD_TOKENS"
 
     if not payload:       
-        return "Payload is missing!"
+        return "BAD_PAYLOAD"
 
     device_tokens = device_tokens.split(',')
     if len(device_tokens) < 1:
-        return "Invalid device token format"
+        return "BAD_TOKENS"
 
     # Format the header of the APN notification
     payload = payload.encode('ascii', errors='ignore')
     apn_data = '!BLLH32sH%ds' % len(payload)
     
-    apn_notification = r''
-    for token in device_tokens:
-        # Strip the token and pack the notification data into a
-        # binary format.
-        token = token.strip().decode('hex')
-        apn_notification += struct.pack(apn_data, 0, provider_identifier, expiry, 32, token, len(payload), payload)
-    
-    safe_send_apn(apn_notification, use_sandbox = use_sandbox)
+    try:
+        apn_notification = r''
+        for token in device_tokens:
+            # Strip the token and pack the notification data into a
+            # binary format.
+            token = token.strip().decode('hex')
+            apn_notification += struct.pack(apn_data, 0, provider_identifier, expiry, 32, token, len(payload), payload)
+        
+        safe_send_apn(apn_notification, use_sandbox = use_sandbox)
+
+    except TypeError:
+        return "BAD_DEVICE"
 
     return "OK"
 
